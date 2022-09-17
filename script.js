70f545cec3c88794003b5d28d66d51f78001da81
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2022-09-17 15:42:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var counter;
 
+
+
 document.getElementById('high-five-button').addEventListener('click', (event) => {
   counter = (typeof counter === 'number' ? counter : 0) + 1;
   let element_click_counter = document.getElementById('click-counter');
