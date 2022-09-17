7877d17d21908ab743c46021ab25bbbe59f384d8
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2022-09-17 14:40:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -69,6 +69,8 @@ prettify.toHTML(run(), console);
 
 var counter;
 
+
+
 document.getElementById('high-five-button').addEventListener('click', (event) => {
   counter = (typeof counter === 'number' ? counter : 0) + 1;
   let element_click_counter = document.getElementById('click-counter');
